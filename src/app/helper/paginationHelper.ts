7eb36d86dfa92170e80b84ec347dba3c1c8ad43c
Tutul Kabir <tutulkabir@gmail.com>
2025-10-20 22:10:59 +0530

    searching, sorting, pagination and filtering completed on users

---
+++ src/app/helper/paginationHelper.ts
@@ -0,0 +1,34 @@
+interface IOptions {
+  page?: string | number;
+  limit?: string | number;
+  sortBy?: string;
+  sortOrder?: string;
+}
+
+interface IOptionsResult {
+  page: number;
+  limit: number;
+  skip: number;
+  sortBy: string;
+  sortOrder: string;
+}
+const calculatePagination = (options: IOptions): IOptionsResult => {
+  const page: number = Number(options.page) || 1;
+  const limit: number = Number(options.limit) || 10;
+  const skip: number = (page - 1) * limit;
+
+  const sortBy: string = options.sortBy || "createdAt";
+  const sortOrder: string = options.sortOrder || "desc";
+
+  return {
+    page,
+    limit,
+    skip,
+    sortBy,
+    sortOrder,
+  };
+};
+
+export const PaginationHelper = {
+  calculatePagination,
+};
